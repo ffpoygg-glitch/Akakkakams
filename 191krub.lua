7c1a857f4d77287f0abd3d2808a21cab2f85d444
ffpoygg-glitch <ffpoygg@gmail.com>
2026-06-03 11:38:11 +0700

    Update 191krub.lua

--- 191krub.lua
+++ 191krub.lua
@@ -10,6 +10,7 @@ local AllowedIDs = {
     [7356705432] = true,  
     [8011136734] = true,
 }
+
 if not AllowedIDs[game:GetService("Players").LocalPlayer.UserId] then
     game:GetService("Players").LocalPlayer:Kick("ใครให้มึงใช้ไอ้เหี้ยบ้านมึงจนหรอไอสัส")
     return
